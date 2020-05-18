4af4423a361f2c0afb04b13cd5a517b87c2d7c4e
Branden HIntz <brandenhintz@gmail.com>
2020-05-17 19:49:34 -0500

    fixed plurality in day(s)

--- todo.js
+++ todo.js
@@ -7,21 +7,33 @@ addItem=()=>{
         var nowms = now.getTime();
         var timeleft = Math.round((dat - nowms)/(1000*60*60*24)) + 1;
 
-        if(item && timeleft>0){
+        if(item && timeleft>0)
+        {
             document.getElementById("todolist").innerHTML = "";
-            if (tasks.length == 0 || timeleft >= tasks[tasks.length-1][1]){
+            if (tasks.length == 0 || timeleft >= tasks[tasks.length-1][1])
+            {
                 tasks.push([item, timeleft]);
-                }
-            else {
-                for (var i = 0; i < tasks.length; i++){
-                    if (timeleft < tasks[i][1]){
+            }
+            else 
+            {
+                for (var i = 0; i < tasks.length; i++)
+                {
+                    if (timeleft < tasks[i][1])
+                    {
                         tasks.splice(i, 0, [item, timeleft])
                         break;
                     }
                 }
             }
             for (var i = 0; i < tasks.length; i++){
-                document.getElementById("todolist").innerHTML += ("<div id=" + i + " role='alert'><div>" + tasks[i][0] + "<span type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden= 'true'>&times;</span></span></div><div>" + tasks[i][1] + " day(s)</div></div>");
+                if (tasks[i][1] == 1)
+                {
+                    document.getElementById("todolist").innerHTML += ("<div id=" + i + " role='alert'><div>" + tasks[i][0] + "<span type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden= 'true'>&times;</span></span></div><div>1 day</div></div>");
+                }
+                else
+                {
+                    document.getElementById("todolist").innerHTML += ("<div id=" + i + " role='alert'><div>" + tasks[i][0] + "<span type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden= 'true'>&times;</span></span></div><div>" + tasks[i][1] + " days</div></div>");
+                }
                 if (tasks[i][1] > 6)
                 {
                     document.getElementById(i).setAttribute("class", " alert alert-secondary alert-dismissible mr-2 task")
@@ -29,7 +41,8 @@ addItem=()=>{
                 else if (tasks[i][1] > 2)
                 {
                     document.getElementById(i).setAttribute("class", "alert alert-warning alert-dismissible mr-2 task")
-                } else 
+                } 
+                else 
                 {
                     document.getElementById(i).setAttribute("class", "alert alert-danger alert-dismissible mr-2 task")
                 }
