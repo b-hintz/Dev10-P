21c6fda485d156b25cc6622d647464a5d61eb9f7
Branden HIntz <brandenhintz@gmail.com>
2020-05-17 19:40:19 -0500

    sorting

--- todo.js
+++ todo.js
@@ -1,4 +1,4 @@
-let counter = 0;
+let tasks = [];
 
 addItem=()=>{
         var item = document.getElementById("input1").value;
@@ -8,20 +8,33 @@ addItem=()=>{
         var timeleft = Math.round((dat - nowms)/(1000*60*60*24)) + 1;
 
         if(item && timeleft>0){
-            document.getElementById("todolist").innerHTML += ("<div id=" + counter + " role='alert'><div>" + item + "<span type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden= 'true'>&times;</span></span></div><div id=" + (counter+1) + ">" + timeleft + " day(s)</div></div>");
-            if (timeleft > 6)
-            {
-                document.getElementById(counter).setAttribute("class", " alert alert-secondary alert-dismissible mr-2 task")
-            } 
-            else if (timeleft > 2)
-            {
-                document.getElementById(counter).setAttribute("class", "alert alert-warning alert-dismissible mr-2 task")
-            } else 
-            {
-                document.getElementById(counter).setAttribute("class", "alert alert-danger alert-dismissible mr-2 task")
+            document.getElementById("todolist").innerHTML = "";
+            if (tasks.length == 0 || timeleft >= tasks[tasks.length-1][1]){
+                tasks.push([item, timeleft]);
+                }
+            else {
+                for (var i = 0; i < tasks.length; i++){
+                    if (timeleft < tasks[i][1]){
+                        tasks.splice(i, 0, [item, timeleft])
+                        break;
+                    }
+                }
             }
-            counter += 2;
+            for (var i = 0; i < tasks.length; i++){
+                document.getElementById("todolist").innerHTML += ("<div id=" + i + " role='alert'><div>" + tasks[i][0] + "<span type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden= 'true'>&times;</span></span></div><div>" + tasks[i][1] + " day(s)</div></div>");
+                if (tasks[i][1] > 6)
+                {
+                    document.getElementById(i).setAttribute("class", " alert alert-secondary alert-dismissible mr-2 task")
+                } 
+                else if (tasks[i][1] > 2)
+                {
+                    document.getElementById(i).setAttribute("class", "alert alert-warning alert-dismissible mr-2 task")
+                } else 
+                {
+                    document.getElementById(i).setAttribute("class", "alert alert-danger alert-dismissible mr-2 task")
+                }
+            }
         }
     document.getElementById("input1").value="";
     document.getElementById("input2").value="";
-    }
+}
